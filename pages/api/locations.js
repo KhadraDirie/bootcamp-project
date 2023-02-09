008687fe60566a339eadf5c11af077f842f5cd81
KhadereDirie <112973040+KhadereDirie@users.noreply.github.com>
2023-02-09 20:04:51 +0000

    cont locations page

---
+++ pages/api/locations.js
@@ -0,0 +1,24 @@
+export default function handler (req,res){
+    const locations = [
+        {
+            "id": "Paris",
+            "title": "Paris",
+            "data": "March 2019",
+            "imgUrl": "/assets/paris.jpg"
+        },
+        {
+            "id": "Bali",
+            "title": "Bali",
+            "author": "August 2019",
+            "imgUrl": "/assets/bali.jpg"
+        },
+        {
+            "id": "New York City",
+            "title": "New York City",
+            "author": "September 2022",
+            "imgUrl": "/assets/nyc.jpg"
+        }
+    ]
+    
+    
+}
