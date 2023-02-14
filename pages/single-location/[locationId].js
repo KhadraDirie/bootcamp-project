9f325ed78c199eeba4a03c60736567fae1839786
KhadereDirie <112973040+KhadereDirie@users.noreply.github.com>
2023-02-14 19:46:32 +0000

    added .env to gitignore

--- pages/single-location/[locationId].js
+++ pages/single-location/[locationId].js
@@ -4,6 +4,7 @@
 
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
+import Hero from "@/components/Homepage/Hero"
 
 //how can we query this  and get it from our list of locations so we can data for each location
 export default function SingleLocation() {
@@ -11,26 +12,44 @@ export default function SingleLocation() {
    const [location, setLocation] = useState(null)
    const {locationId} =router.query; //destructing
     
-// the usEffect hook makes a request to the API end point so that we can get a particular location so we can use it in a component
-    useEffect(() =>{
-        getLocation(locationId) //when component is rendered is going to get a location based on the id that is passed in and will set it to the state
-   },[locationId])
+// the usEffect hook makes a request to the API end point so that we can get a particular location and use it in a component
+    useEffect(() => {
+      if(!locationId) return;
+
+        getLocation(locationId);  
+   },[locationId]);
 
    const getLocation = async(id) =>{
    const response = await fetch (`/api/location-by-id?id=${id}`);
    const data =  await response.json();
-   const {location} = data;
+   const { location } = data;
    setLocation(location);
 
- }
+ } 
+  console.log(location);
+  
+  if (!location){
+  return(
+    <div className='text-white'>Loading...</div>
 
- console.log(location)
+  )
+}
+
 
 
   return (
-    <div className='text-white'>
-      Single Location Page
-    </div>
+
+    <div className='text-white'> 
+
+
+    <Hero
+      imgUrl ={location.imgUrl}
+      title = {location.title}
+      date = {location.date}
+    />
+    
+</div>
+
   )
 }
 
