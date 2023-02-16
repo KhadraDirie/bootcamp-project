5ad2d25fc53707f4b770287876bfe3c276c6eee3
KhadereDirie <112973040+KhadereDirie@users.noreply.github.com>
2023-02-16 01:39:01 +0000

    add styling

--- pages/single-location/[locationId].js
+++ pages/single-location/[locationId].js
@@ -3,8 +3,6 @@
 //we can access locationId depending on what the user clicks 
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-import Hero from "@/components/Homepage/Hero"
-
 
 
 
@@ -12,6 +10,7 @@ export default function SingleLocation() {
    const router = useRouter();
    const [location, setLocation] = useState(null)
    const {locationId} =router.query; //destructing
+ 
     
 // the usEffect hook makes a request to the API end point so that we can get a particular location and use it in a component
     useEffect(() => {
@@ -24,14 +23,15 @@ export default function SingleLocation() {
   try {
     const response = await fetch(`/api/location-by-id?id=${id}`);
     const data = await response.json();
-    const {location} = data;
-    setLocation(location);
+    const {locations} = data;
+   
+    setLocation(locations);
   } catch (error) {
     console.log(error);
   
   }
 }
-  console.log(location);
+
   
   if (!location){
   return(
@@ -41,17 +41,19 @@ export default function SingleLocation() {
 }
 
   return (
-
     <div className='text-white'> 
-
-
-    <Hero
-      imgUrl ={location.imgUrl}
-      title = {location.title}
-      date = {location.date}
-    />
+    <h1 className='text-5xl text-white font-semibold py-2 m-8'>Single Location Page</h1>
+  
+      <div>
+        <img className='w-1/2 h-1/4 m-8 py-2'  src ={location.img_url} alt={location.title} />
+        <h1 className='text-4xl m-8'>{location.title}</h1>
+        <p className='text-2xl m-8 py-2'>{location.date}</p>
+      </div>
+      <button className='py-2 m-8 bg-purple-500 hoover:bg-purple-700 text-white font-bold px-4 rounded'>Back to Homepage</button>
+   
+   </div>
     
-</div>
+
 
   )
 }
