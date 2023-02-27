25775c42a8aebf0cbe9a632aa93d999719a488cf
KhadereDirie <112973040+KhadereDirie@users.noreply.github.com>
2023-02-27 22:36:26 +0000

    .

--- components/search/location-item.js
+++ components/search/location-item.js
@@ -1,67 +1,14 @@
-import Input from "../form/input"
-import Header from "../shared/header"
-import Content from "../shared/content"
-import Footer from "../shared/footer"
-import { useEffect, useState } from "react"
 
-export default function SearchLocation(){
-    const[locations,setLocations] = useState([])
-    const [input,setInput] = useState("");
+export default function LocationItem({ location }) {
+    const { id, title, img_url, visited_date } = location;
 
-    useEffect(() =>{
-        getLocations();
-    },[]);
-        
-    const getLocations = async () =>{
-        try {
-            const response = await fetch('./api/search-locations');
-            const data = await response.json();
-            setLocations(data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-
-
-    return(
-        <div className="w-full">
-            <div className="tracking-widest text-5xl title-font font-medium text-black mb-1 m-5">
-                <Header name="Search" />
+    return (  
+        <div key={id} className="w-full h-40 bg-red-100 relative">
+            <img src={img_url} className="w-full h-full brightness-75" />
+            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+                <p className="text-black font-bold text-3xl">{title}</p>
+               < p className="text-black font-bold text-3xl">{visited_date}</p>
             </div>
-            <Content> 
-                <div className=" w-full md:w-6/12  lg:4-4/12 flex flex-row space-x-2"> 
-                    <Input 
-                    value = {input}
-                    onChange ={value => setInput(value)} //everytime something is typed in the search input the value set in state (which is an empty string) updates to whatever is typed in the input
-                    />
-                    <button className="bg-rose-500 text-white px-6 py-2 rounded-md"
-                    type ="button" 
-                    
-                    >
-                        search
-                    </button>
-                   
-                </div>
-
-                <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
-  {locations.map((location) => (
-    <div key={location.id} className="w-full bg-rose-200 h-40"></div>
-  ))}
-</div>
-
-                  
-
-               
-            </Content>
-
-            <Footer title = "Go to contact"  href = "/contact"/>
-
-
-            
-
-            
         </div>
     )
-    }
-
+}
--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,68 @@
-import SearchLocation from "../components/search/location-item"
+import Input from "../components/form/input"
+import LocationItem from "@/components/search/location-item";
+import Content from "@/components/shared/content";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header"
+import { useEffect, useState } from "react"
 
-export default function Search (){
+export default function SearchLocation(){
+    const[locations,setLocations] = useState([])
+    const [input,setInput] = useState("");
+
+    useEffect(() =>{
+        getLocations();
+    },[]);
+        
+    const getLocations = async () =>{
+        try {
+            const response = await fetch('./api/search-locations');
+            const data = await response.json();
+            setLocations(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+
     return(
         <div className="w-full">
-            <SearchLocation/>
+            <div className="tracking-widest text-5xl title-font font-medium text-black mb-1 m-5">
+                <Header name="Search" />
+            </div>
+            <Content> 
+                <div className=" w-full md:w-6/12  lg:4-4/12 flex flex-row space-x-2"> 
+                    <Input 
+                    value = {input}
+                    onChange ={value => setInput(value)} //everytime something is typed in the search input the value set in state (which is an empty string) updates to whatever is typed in the input
+                    />
+                    <button className="bg-rose-500 text-white px-6 py-2 rounded-md"
+                    type ="button" 
+                    
+                    >
+                        search
+                    </button>
+                   
+                </div>
+
+                <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
+  {locations.map((location) => (
+
+ <LocationItem key={location.id} location={location} />  ))}
+</div>
+
+                  
+
+               
+            </Content>
+
+            <Footer title = "Go to contact"  href = "/contact"/>
 
 
             
 
+            
         </div>
     )
-}
+    }
+
